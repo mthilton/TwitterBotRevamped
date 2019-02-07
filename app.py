2c1516e709d001f4ef278ddb703ed1ac6a0873b0
Matthew Hilton <mthilton@ucsc.edu>
2019-02-07 14:10:21 -0800

    Fixed token timeout errors

--- app.py
+++ app.py
@@ -22,10 +22,29 @@ testfile.close()
 def grabFromPayload(results):
     return results["item"]["name"], results["item"]["external_urls"]["spotify"], results["progress_ms"], results["item"]["duration_ms"], results["item"]["uri"]
 
-def calcSleep(sp, slp_time, prev_tr_uri):
-    results = sp.current_user_playing_track()
-    tr_name, tr_link, cur_tr_prog, tr_len, cur_tr_uri = grabFromPayload(results)
+def refeshSpotifyToken():
+    scope = 'user-read-currently-playing'
+    token = util.prompt_for_user_token(username=e.spot_username,
+                                        scope=scope,
+                                        client_id=e.spot_client_id,
+                                        client_secret= e.spot_client_secret,
+                                        redirect_uri=e.spot_redirect_uri)
 
+    # On success grab the current user playing track
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        return sp.current_user_playing_track()
+
+    # Otherwise authentication failed and we will kill the program
+    else:
+        print ("Can't get token for " + e.spot_username)
+        exit()
+
+
+def calcSleep(slp_time, prev_tr_uri):
+
+    results = refeshSpotifyToken()
+    tr_name, tr_link, cur_tr_prog, tr_len, cur_tr_uri = grabFromPayload(results)
 
     if (cur_tr_uri == prev_tr_uri):
         if ((cur_tr_prog/1000) > 10):
@@ -62,104 +81,89 @@ def mainLoop():
                             access_token_secret=e.twit_access_token_secret)
 
         # Spotify Authentication
-        scope = 'user-read-currently-playing'
-        token = util.prompt_for_user_token(username=e.spot_username,
-                                            scope=scope,
-                                            client_id=e.spot_client_id,
-                                            client_secret= e.spot_client_secret,
-                                            redirect_uri=e.spot_redirect_uri)
+        results = refeshSpotifyToken()
 
-        # On success grab the current user playing track
-        if token:
+        # Grab relavent information from the payload if there are results
+        if(results != None):
 
-                sp = spotipy.Spotify(auth=token)
+            artist_query = []
+            tr_name, tr_link, cur_tr_prog, tr_len, cur_tr_uri = grabFromPayload(results)
 
-                results = sp.current_user_playing_track()
+            # Fetching the Artists in on the song and putting them into a list
+            num_artists = len(results["item"]["artists"])
 
-                # Grab relavent information from the payload if there are results
-                if(results != None):
+            for i in range(num_artists):
+                artist_query.append(results["item"]["artists"][i]["name"])
 
-                    artist_query = []
-                    tr_name, tr_link, cur_tr_prog, tr_len, cur_tr_uri = grabFromPayload(results)
+            # If the song is currently playing, its been playing for longer than
+            # half of its duration, and its not the previous track, then Tweet it out
+            if (results["is_playing"] and
+                tr_len/2 < cur_tr_prog and
+                cur_tr_uri != prev_tr_uri):
 
-                    # Fetching the Artists in on the song and putting them into a list
-                    num_artists = len(results["item"]["artists"])
+                # Catching Twitter Error
+                try:
+                    tr_artist = tf.lookup_user(twit = twit, query = artist_query)
+                    status = twit.PostUpdate("Current Track: " + tr_name + "\nArtists: " + tr_artist + "\nListen now at: " + tr_link)
+                    tweet_info = str("Tweet: " + status.text + "\nTweet ID: " + status.id_str + "\nTimestamp: " + status.created_at + "\n")
+                    print(tweet_info)
+                    with open(sys.argv[1], "a") as log:
+                        log.write("Sucessful Tweet!\n")
+                        log.write(tweet_info)
+                        log.write("--------------------------------------------------------------------------\n")
+                    prev_tr_uri = cur_tr_uri
 
-                    for i in range(num_artists):
-                        artist_query.append(results["item"]["artists"][i]["name"])
+                except twitter.error.TwitterError as err:
+                    print("This song has already been tweeted!\n" + repr(err))
 
-                    # If the song is currently playing, its been playing for longer than
-                    # half of its duration, and its not the previous track, then Tweet it out
-                    if (results["is_playing"] and
-                        tr_len/2 < cur_tr_prog and
-                        cur_tr_uri != prev_tr_uri):
+                except twitfunc.InvalidTwitterAuthError as err:
+                    print(err)
 
-                        # Catching Twitter Error
-                        try:
-                            tr_artist = tf.lookup_user(twit = twit, query = artist_query)
-                            status = twit.PostUpdate("Current Track: " + tr_name + "\nArtists: " + tr_artist + "\nListen now at: " + tr_link)
-                            tweet_info = str("Tweet: " + status.text + "\nTweet ID: " + status.id_str + "\nTimestamp: " + status.created_at + "\n")
-                            print(tweet_info)
-                            with open(sys.argv[1], "a") as log:
-                                log.write("Sucessful Tweet!\n")
-                                log.write(tweet_info)
-                                log.write("--------------------------------------------------------------------------\n")
-                            prev_tr_uri = cur_tr_uri
+                slp_time = (tr_len - cur_tr_prog) / 1000
+                slp_time = math.ceil(slp_time)
+                print("Sleeping for {} seconds!".format(slp_time))
+                while slp_time > 0:
+                    slp_time, prev_tr_uri = calcSleep(slp_time, prev_tr_uri)
 
-                        except twitter.error.TwitterError as err:
-                            print("This song has already been tweeted!\n" + repr(err))
+            # If the song is not playing then inform the client console
+            elif (results["is_playing"] == False):
+                print("Not currently playing anything!")
+                print("Sleeping for 20 seconds!")
+                time.sleep(20)
 
-                        except twitfunc.InvalidTwitterAuthError as err:
-                            print(err)
+            # If the current song uri is equal to the previous song uri, then
+            # Inform the client console
+            elif (cur_tr_uri == prev_tr_uri):
+                print("The current song is still the previous song!")
+                slp_time = (tr_len - cur_tr_prog) / 1000
+                if slp_time < 1:
+                    slp_time = 2
 
-                        slp_time = (tr_len - cur_tr_prog) / 1000
-                        slp_time = math.ceil(slp_time)
-                        print("Sleeping for {} seconds!".format(slp_time))
-                        while slp_time > 0:
-                            slp_time, prev_tr_uri = calcSleep(sp, slp_time, prev_tr_uri)
+                slp_time = math.ceil(slp_time)
+                print("Sleeping for {} seconds!".format(slp_time))
+                while slp_time > 0:
+                    slp_time, prev_tr_uri = calcSleep(slp_time, prev_tr_uri)
 
-                    # If the song is not playing then inform the client console
-                    elif (results["is_playing"] == False):
-                        print("Not currently playing anything!")
-                        print("Sleeping for 20 seconds!")
-                        time.sleep(20)
+            # Otherwise, inform the client console that the user hasn't listened
+            # to at least half of the song.
+            else:
+                prev_tr_uri = cur_tr_uri
+                print("Have not listened to enough of the song!")
+                hwp = (3 * tr_len)/4
+                slp_time = (hwp - cur_tr_prog) / 1000
+                slp_time = math.ceil(slp_time)
+                print("Sleeping for {} seconds!".format(slp_time))
+                while slp_time > 0:
+                    slp_time, prev_tr_uri = calcSleep(slp_time, prev_tr_uri)
 
-                    # If the current song uri is equal to the previous song uri, then
-                    # Inform the client console
-                    elif (cur_tr_uri == prev_tr_uri):
-                        print("The current song is still the previous song!")
-                        slp_time = (tr_len - cur_tr_prog) / 1000
-                        if slp_time < 1:
-                            slp_time = 2
-
-                        slp_time = math.ceil(slp_time)
-                        print("Sleeping for {} seconds!".format(slp_time))
-                        while slp_time > 0:
-                            slp_time, prev_tr_uri = calcSleep(sp, slp_time, prev_tr_uri)
-
-                    # Otherwise, inform the client console that the user hasn't listened
-                    # to at least half of the song.
-                    else:
-                        prev_tr_uri = cur_tr_uri
-                        print("Have not listened to enough of the song!")
-                        hwp = (3 * tr_len)/4
-                        slp_time = (hwp - cur_tr_prog) / 1000
-                        slp_time = math.ceil(slp_time)
-                        print("Sleeping for {} seconds!".format(slp_time))
-                        while slp_time > 0:
-                            slp_time, prev_tr_uri = calcSleep(sp, slp_time, prev_tr_uri)
-
-                else:
-                    print("No user currently logged in, sleeping for 60 seconds!")
-                    time.sleep(60)
-
-        # Otherwise authentication failed and we will kill the program
         else:
-            print ("Can't get token for " + username)
-            exit()
+            print("No user currently logged in, sleeping for 60 seconds!")
+            time.sleep(60)
+
+
 
 try:
-    mainLoop()
+    t = mainLoop(t)
 except Exception as e:
     with open(sys.argv[1], "a") as log:
         currTime = datetime.datetime.now()
