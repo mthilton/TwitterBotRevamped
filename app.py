16888dabb14a776e85975876ac9a7193b1be8dc9
Matthew Hilton <mthilton@ucsc.edu>
2019-02-20 04:29:45 -0800

    Reverting achange with a new change. (im tired im sorry

--- app.py
+++ app.py
@@ -180,6 +180,7 @@ def mainLoop():
             # Otherwise, inform the client console that the user hasn't listened
             # to at least half of the song.
             elif cur_tr_prog < (3 * tr_len)/4:
+                prev_tr_uri = cur_tr_uri
                 currTime = datetime.datetime.now()
                 currTime = currTime.strftime("%Y-%m-%d-%X")
                 print("[{}] Have not listened to enough of the song!".format(str(currTime)))
